f79444d634f36030f84693a35850ae1fb7d0819d
jinfagang <nicholasjela@gmail.com>
2018-01-14 20:31:18 +0800

    finish cabinet v1

--- install_cabinet.sh
+++ install_cabinet.sh
@@ -1,2 +1,19 @@
 # !/usr/bin/sh
-sudo apt install -y libcurl4-openssl-dev
+sudo chmod -R 777 *
+if [ "$(uname)" == "Darwin" ]; then
+    # Do something under Mac OS X platform
+    sudo cp bin/cab_macos /usr/local/bin/cab
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # Do something under GNU/Linux platform
+    sudo apt install -y libcurl4-openssl-dev
+    sudo cp bin/cab_linux /usr/local/bin/cab
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    # Do something under 32 bits Windows NT platform
+    echo "this is windows 32bit"
+    
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    # Do something under 64 bits Windows NT platform
+    echo "this is windows 64bit"
+fi
+
+echo "You just installed cabinet - ultimate tool box!"
--- src/blog.rs
+++ src/blog.rs
@@ -1,21 +1,70 @@
 extern crate colored;
-extern crate time;
 extern crate chrono;
 
 
 use colored::*;
-use chrono::prelude::*;
+use self::chrono::Local;
+
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+
 
 
 pub fn generate_blog_template(use_date: bool, title: &str) {
-    if use_date {
-        println!("\nuse date as title prefix {}", "ON".yellow().bold());
 
-        let now = Utc::now();
-        println!("{}", now.format("%b %-d, %-I:%M").to_string());
-        println!("generate blog with name: {}", title)
+    let mut save_file_title = str::replace(title, " ", "_").to_string();
+
+    let date = Local::now();
+    let date_str = date.format("%Y_%m_%d_%H").to_string();
+    let date_time_str = date.format("%Y-%m-%d %H:%M:%S").to_string();
+
+    if use_date {
+        println!("\n=> use date as title prefix {}", "ON".yellow().bold());
+        save_file_title = format!("{}_{}.md", date_str, save_file_title);
     } else {
-        println!("\nuse date as title prefix {}", "OFF".yellow().bold());
-        println!("generate blog with name: {}", title)
+        println!("\n=> use date as title prefix {}", "OFF".yellow().bold());
+        save_file_title = format!("{}.md", save_file_title);
     }
+
+    println!("=> generate blog with name: {:?}", save_file_title);
+
+    let content = format!("---
+title: {}
+date: {}
+category: 默认分类
+---
+本文介绍 {}
+<!-- more -->
+
+# {}
+> This article was original written by Jin Tian, welcome re-post, first come with https://jinfagang.github.io . but please keep this copyright info, thanks, any question could be asked via wechat: `jintianiloveu` ",
+                          title, date_time_str, title, title);
+
+    let path = Path::new(&save_file_title);
+    let display = path.display();
+    let _file = match File::open(&path) {
+        Err(_) => {
+
+            let mut file = match File::create(&path) {
+                Err(why) => panic!("=> couldn't create {}: {}",
+                                   display,
+                                   why.description()),
+                Ok(mut file) => {
+                    match file.write_all(content.as_bytes()) {
+                        Err(why) => {
+                            panic!("=> couldn't write to {}: {}", display,
+                                   why.description().red())
+                        },
+                        Ok(_) => println!("=> successfully wrote to {}", display.to_string().yellow().bold()),
+                    }
+                },
+            };
+        },
+        Ok(file) => {
+            println!("=> file already exist!")
+        },
+    };
 }
