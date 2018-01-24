1248b56c78d24720b854c24809a8f0a03b2a8800
jinfagang <nicholasjela@gmail.com>
2018-01-24 13:59:21 +0800

    fix bash install problem

--- install_cabinet.sh
+++ install_cabinet.sh
@@ -1,3 +1,19 @@
+<<<<<<< HEAD
+# !/bin/bash
+sudo chmod -R 777 *
+if [ "$(uname)" = "Darwin" ]; then
+    # Do something under Mac OS X platform
+    sudo cp bin/cab_macos /usr/local/bin/cab
+elif [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]; then
+    # Do something under GNU/Linux platform
+    sudo apt install -y libcurl4-openssl-dev
+    sudo cp bin/cab_linux /usr/local/bin/cab
+elif [ "$(expr substr $(uname -s) 1 10)" = "MINGW32_NT" ]; then
+    # Do something under 32 bits Windows NT platform
+    echo "this is windows 32bit"
+    
+elif [ "$(expr substr $(uname -s) 1 10)" = "MINGW64_NT" ]; then
+=======
 # !/usr/bin/sh
 sudo chmod -R 777 *
 if [ "$(uname)" == "Darwin" ]; then
@@ -12,6 +28,7 @@ elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
     echo "this is windows 32bit"
     
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+>>>>>>> a3f411bf2700e9fd79eba2db9ca2b6210334d3c1
     # Do something under 64 bits Windows NT platform
     echo "this is windows 64bit"
 fi
