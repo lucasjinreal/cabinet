76adc16a02c3f0392b7726d94f3885e20d7c95a8
jinfagang <nicholasjela@gmail.com>
2018-01-24 14:00:53 +0800

    add

--- install_cabinet.sh
+++ install_cabinet.sh
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 # !/bin/bash
 sudo chmod -R 777 *
 if [ "$(uname)" = "Darwin" ]; then
@@ -13,22 +13,6 @@ elif [ "$(expr substr $(uname -s) 1 10)" = "MINGW32_NT" ]; then
     echo "this is windows 32bit"
     
 elif [ "$(expr substr $(uname -s) 1 10)" = "MINGW64_NT" ]; then
-=======
-# !/usr/bin/sh
-sudo chmod -R 777 *
-if [ "$(uname)" == "Darwin" ]; then
-    # Do something under Mac OS X platform
-    sudo cp bin/cab_macos /usr/local/bin/cab
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    # Do something under GNU/Linux platform
-    sudo apt install -y libcurl4-openssl-dev
-    sudo cp bin/cab_linux /usr/local/bin/cab
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-    # Do something under 32 bits Windows NT platform
-    echo "this is windows 32bit"
-    
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
->>>>>>> a3f411bf2700e9fd79eba2db9ca2b6210334d3c1
     # Do something under 64 bits Windows NT platform
     echo "this is windows 64bit"
 fi
